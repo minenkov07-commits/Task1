4df4503f8f059231285eba374f83bfde11757687
minenkov07-commits <minenkov.07@mail.ru>
2025-09-14 15:06:24 +0300

    Create Строки и стек.py

---
+++ Строки и стек.py
@@ -0,0 +1,17 @@
+single_quotes = 'Привет, мир!'
+double_quotes = "Python - это здорово!"
+first_name = "Иван"
+last_name = "Петров"
+full_name = first_name + " " + last_name
+s = "Python"
+print(s * 3)
+
+
+//
+
+
+stack = Stack()  
+stack.push(1)  
+stack.push(2)  
+print(stack.peek())   
+print(stack.pop())  
